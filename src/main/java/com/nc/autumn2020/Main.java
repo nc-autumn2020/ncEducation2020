ebbcbd1a255273ad4fbcfcd745b8a6bafa6728a9
anmi0217 <andrei.mishin@netcracker.com>
2020-10-23 19:48:22 +0400

    solution implementation example

--- src/main/java/com/nc/autumn2020/Main.java
+++ src/main/java/com/nc/autumn2020/Main.java
@@ -1,15 +1,10 @@
 package com.nc.autumn2020;
 
-import com.nc.autumn2020.lesson3.Human;
+import com.nc.autumn2020.solutions.lesson3.Solution1;
 
 public class Main {
     public static void main(String[] args) {
-
-        Human human = new Human.Builder(1000,"anmi","anotherName").
-                age((short) 11).
-                job("have work").
-                build();
-        System.out.println(human);
+        new Solution1().run(args);
 
     }
 }
--- src/main/java/com/nc/autumn2020/solutions/lesson3/Solution1.java
+++ src/main/java/com/nc/autumn2020/solutions/lesson3/Solution1.java
@@ -1,4 +1,14 @@
 package com.nc.autumn2020.solutions.lesson3;
 
+import com.nc.autumn2020.lesson3.Human;
+
 public class Solution1 {
+    public void run(String[] args){
+        System.out.println("some solution implementation example");
+        Human human = new Human.Builder(1000,"anmi","anotherName").
+                age((short) 11).
+                job("have work").
+                build();
+        System.out.println(human);
+    }
 }
